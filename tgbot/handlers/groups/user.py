40957ffabf36479519396915ee1361e3e78f612e
Diana Tikhonova <molodkova1996@mail.ru>
2022-11-23 10:10:06 +0300

    Keyboard for private was removed from group chat as response on command start

--- tgbot/handlers/groups/user.py
+++ tgbot/handlers/groups/user.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ChatType, ReplyKeyboardRemove
 
 from tgbot.keyboards.reply.for_private import main_keyboard
 
@@ -7,9 +7,12 @@ from tgbot.keyboards.reply.for_private import main_keyboard
 async def user_start(message: Message) -> None:
     """ Хендлер для команды start (префикс команды '/' или '!') """
 
-    await message.answer(f'Привет, {message.from_user.first_name}!', reply_markup=main_keyboard())
+    if message.chat.type == ChatType.PRIVATE:
+        await message.answer(f'Привет, {message.from_user.first_name}!', reply_markup=main_keyboard())
+    else:
+        await message.answer(f'Привет, {message.from_user.first_name}!', reply_markup=ReplyKeyboardRemove())
 
 
 def register_user(dp: Dispatcher):
-    dp.register_message_handler(user_start, commands=["start"], commands_prefix='/!', state="*")
+    dp.register_message_handler(user_start, commands=["start"], commands_prefix='/!', state="*", )
 
