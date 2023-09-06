ca9917fbfeb76e0afd40652f92f6ea72a2df57da
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-09-06 13:05:40 +0300

    version beta 1.2 bag_list[None]
    # del bugs
    # add fun dict_pop_executor

--- tgbot/utils/captcha_check.py
+++ tgbot/utils/captcha_check.py
@@ -19,9 +19,6 @@ async def check_captcha(call: CallbackQuery, config: Config):
     password: str = call.data.split(':')[1]
     user_id: int = int(call.from_user.id)
     chat_id: int = int(call.message.chat.id)
-    new_user_id: int = int(*user_dict.keys())
-    if new_user_id is not user_id:
-        user_id = new_user_id
     captcha_flag_user_dict.update({user_id: False})
     minute_delta: int = config.time_delta.minute_delta
     if password.isdigit():
@@ -31,13 +28,13 @@ async def check_captcha(call: CallbackQuery, config: Config):
             await call.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id,
                                                 permissions=ChatPermissions(can_send_messages=True),
                                                 until_date=timedelta(seconds=minute_delta))
-            logger.info(f"User {user_id} was pass")
+            logger.info(f"User id:{user_id} name:{call.from_user.full_name}was pass")
             captcha_flag_user_dict.update({user_id: True})
             bot_user: User = await call.message.bot.get_me()
             greeting: str = greeting_text(call=call, bot_user=bot_user)
             msg: Message = await call.message.answer(text=greeting, disable_web_page_preview=True,
                                                      reply_markup=ReplyKeyboardRemove())
-            logger.info(f"New User {user_id} was greeting")
+            logger.info(f"New User id:{user_id} name:{call.from_user.full_name} was greeting")
             await asyncio.sleep(config.time_delta.time_rise_asyncio_del_msg)
             await msg.delete()
 
@@ -47,11 +44,12 @@ async def check_captcha(call: CallbackQuery, config: Config):
             await call.message.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id,
                                                         permissions=ChatPermissions(can_send_messages=False),
                                                         until_date=timedelta(seconds=minute_delta * 4))
-            logger.info(f"User {user_id} was mute seconds = {minute_delta * 4}")
+            captcha_flag_user_dict.update({user_id: True})
+            logger.debug(f"User id:{user_id} name:{call.from_user.full_name} was mute seconds = {minute_delta * 4}")
     else:
         await call.answer(text="wrong answer!\n"
                                "sit in the corner 1 min", show_alert=True)
         await call.message.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id,
                                                     permissions=ChatPermissions(can_send_messages=False),
                                                     until_date=timedelta(seconds=minute_delta))
-        logger.info(f"User {user_id} was mute seconds = {minute_delta}")
+        logger.debug(f"User id:{user_id} name:{call.from_user.full_name} was mute seconds = {minute_delta}")
