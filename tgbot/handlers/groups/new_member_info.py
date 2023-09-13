b8a0bee221d3e2e9a90c8ed21ffb86a17b628cdf
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-09-13 10:58:43 +0300

    del content_type

--- tgbot/handlers/groups/new_member_info.py
+++ tgbot/handlers/groups/new_member_info.py
@@ -24,5 +24,4 @@ async def new_member_info(message: Message) -> None:
 
 
 def register_new_member_info(dp: Dispatcher):
-    dp.register_message_handler(new_member_info,
-                                content_types=ContentTypes.NEW_CHAT_MEMBERS)
+    dp.register_chat_member_handler(new_member_info)
