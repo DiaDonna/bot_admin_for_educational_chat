8dfe855484b51669e3b16ca225f89da35aafaa77
konstantin_liapin <lajpinkonstantin@gmail.com>
2023-10-12 12:52:01 +0300

    fix some types

--- tgbot/utils/admin_ids.py
+++ tgbot/utils/admin_ids.py
@@ -1,16 +1,16 @@
 from aiogram.types import Message, ChatMember
-from typing import List
+
 
 from tgbot.config import Config
 
 
-async def get_admins_ids_for_report(message: Message, config: Config) -> List[int]:
+async def get_admins_ids_for_report(message: Message, config: Config) -> list[int]:
     """
     Возвращает id админов группы, с id владельца или без него,
     зависит от параметра send_report_to_owner
     """
     send_report_to_owner: bool = config.tg_bot.send_report_to_owner
-    admins: List[int] = await get_admins_ids_for_help_and_paste(message)
+    admins: list[int] = await get_admins_ids_for_help_and_paste(message)
     owner: int = await _get_owner_id(message)
     if not send_report_to_owner:
         admins.remove(owner)
@@ -18,11 +18,11 @@ async def get_admins_ids_for_report(message: Message, config: Config) -> List[in
     return admins
 
 
-async def get_admins_ids_for_help_and_paste(message: Message) -> List[int]:
+async def get_admins_ids_for_help_and_paste(message: Message) -> list[int]:
     """
     Возвращает id админов группы, включая владельца группы
     """
-    admins: List[ChatMember] = await _get_all_admins(message)
+    admins: list = await _get_all_admins(message)
 
     return [
         admin.user.id
@@ -34,7 +34,7 @@ async def get_admins_ids_for_help_and_paste(message: Message) -> List[int]:
 async def _get_owner_id(message) -> int:
     """ Возвращает id владельца группы """
 
-    admins: List[ChatMember] = await _get_all_admins(message)
+    admins: list[ChatMember] = await _get_all_admins(message)
     return [
             admin.user.id
             for admin in admins
@@ -42,10 +42,10 @@ async def _get_owner_id(message) -> int:
         ][0]
 
 
-async def _get_all_admins(message: Message) -> List[ChatMember]:
+async def _get_all_admins(message: Message) -> list[ChatMember]:
     """ Возвращает список всех не ботов администраторов группы """
 
-    admins: List[ChatMember] = await message.bot.get_chat_administrators(message.chat.id)
+    admins: list[ChatMember] = await message.bot.get_chat_administrators(message.chat.id)
 
     return [
         admin
